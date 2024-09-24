5ebfb0322854a8237dbc2da64aac6e9328978559
Young5097 <bigfaith0301@naver.com>
2024-09-24 20:07:06 +0900

    [feature] ORM JPA 1장 실습

---
+++ backend/JPA/JPA_Study/src/main/java/org/jpa_study/JpaStudyApplication.java
@@ -0,0 +1,23 @@
+package org.jpa_study;
+
+import org.jpa_study.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudyApplication implements CommandLineRunner {
+
+    @Autowired
+    MemberService memberService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaStudyApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        memberService.insertMember();
+    }
+}
---
+++ backend/JPA/JPA_Study/src/main/java/org/jpa_study/service/MemberService.java
@@ -0,0 +1,59 @@
+package org.jpa_study.service;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.transaction.Transactional;
+import org.jpa_study.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+    @Autowired
+    EntityManagerFactory emf;
+
+    public void insertMember() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin(); //트랜잭션 시작
+            logic(em);  //비즈니스 로직
+            tx.commit();//트랜잭션 커밋
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+    }
+
+    public void logic(EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("지한");
+        member.setAge(2);
+
+        //등록
+        em.persist(member);
+
+        //수정
+        member.setAge(20);
+
+        //한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
+
+        //목록 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("members.size=" + members.size());
+
+        //삭제
+        em.remove(member);
+    }
+}
