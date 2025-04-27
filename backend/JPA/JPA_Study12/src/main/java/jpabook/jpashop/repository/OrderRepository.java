619ab6813f3ef44c59216cab995728ff892cf183
Young5097 <bigfaith0301@naver.com>
2025-04-27 19:42:26 +0900

    ORM 표준 JPA 12장 실습 - 스프링 데이터 JPA 리팩토링

--- backend/JPA/JPA_Study12/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ backend/JPA/JPA_Study12/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.dto.OrderSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -15,46 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class OrderRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public void save(Order order) {
-        em.persist(order);
-    }
-
-    public Order findOne(Long id) {
-        return em.find(Order.class, id);
-    }
-
-    public List<Order> findAll(OrderSearch orderSearch) {
-        String jpql = "select o from Order o join o.member m where 1=1";
-
-        // 주문 상태 검색
-        if (orderSearch.getOrderStatus() != null) {
-            jpql += " and o.orderStatus = :orderStatus";
-        }
-
-        // 회원 이름 검색
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            jpql += " and m.name like :memberName";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class);
-
-        // 주문 상태 파라미터 설정
-        if (orderSearch.getOrderStatus() != null) {
-            query.setParameter("orderStatus", orderSearch.getOrderStatus());
-        }
-
-        // 회원 이름 파라미터 설정
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            query.setParameter("memberName", "%" + orderSearch.getMemberName() + "%");
-        }
-
-        // 결과 반환
-        return query.setMaxResults(1000).getResultList();
-    }
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
