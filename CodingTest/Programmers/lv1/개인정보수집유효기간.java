a8f0dc80be518742dbf27381be9360e20e20dbbc
김영신 <yskim@realsecu.net>
2025-07-28 19:05:54 +0900

    주석 추가가

--- CodingTest/Programmers/lv1/개인정보수집유효기간.java
+++ CodingTest/Programmers/lv1/개인정보수집유효기간.java
@@ -37,7 +37,7 @@ class 개인정보수집유효기간 {
         int year = Integer.parseInt(parts[0]);
         int month = Integer.parseInt(parts[1]);
         int day = Integer.parseInt(parts[2]);
-        // year >= 2000
+        // year >= 2000, month는 1월부터 시작하므로 6월일 경우, 5개월이 흐른거임, 그래서 -1 해줘야함
         return (year - 2000) * 12 * 28 + (month - 1) * 28 + day;
     }
 }
