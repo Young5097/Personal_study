e9a4b14b0ad26440f790f3850f84a0fa008778d9
김영신 <yskim@realsecu.net>
2025-07-30 17:56:43 +0900

    코딩테스트/2025.07.30
    프로그래머스 lv1 sql 4문제

---
+++ CodingTest/Programmers/lv1/물고기평균길이.sql
@@ -0,0 +1,2 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/293259
+SELECT ROUND(AVG(IFNULL(LENGTH,10)), 2) AS AVERAGE_LENGTH FROM FISH_INFO
---
+++ CodingTest/Programmers/lv1/한해잡은물고기수.sql
@@ -0,0 +1,2 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/298516
+SELECT count(*)AS FISH_COUNT FROM FISH_INFO WHERE YEAR(TIME) = 2021;
